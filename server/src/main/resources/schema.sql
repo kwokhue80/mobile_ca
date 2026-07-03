05e108e9b097ae7d9712090f73655a3e920d94fb
Codemelia <ameliaw2903@gmail.com>
2026-07-03 10:31:04 +0800

    ADD user profile entity

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id                  CHAR(36)        NOT NULL,
+    user_id                  CHAR(36)        NOT NULL,
     email_address       VARCHAR(100)    NOT NULL,
     password_hash       VARCHAR(255)    NOT NULL,
     `enabled`           BOOLEAN         NOT NULL        DEFAULT TRUE,
     created_at          DATETIME        NOT NULL        DEFAULT CURRENT_TIMESTAMP,
     updated_at          DATETIME        NOT NULL        DEFAULT CURRENT_TIMESTAMP       ON UPDATE CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (id),
+    PRIMARY KEY (user_id),
     CONSTRAINT uk_users_email_address   UNIQUE (email_address)
 );
